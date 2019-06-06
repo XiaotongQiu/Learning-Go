6b3e1204ea58e1b4552bdee4de48e4f7163f81da
xq2174 <xq2174@columbia.edu>
2019-06-06 13:48:11 -0700

    hello world with string processing

--- hello.go
+++ hello.go
@@ -1,7 +1,19 @@
-package main
+/* package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Hello, world.")
+} */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/XiaotongQiu/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
